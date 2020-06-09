7a5c17e529bce1b0398c9267d91ddd402d61595d
Ismael Cardenas <inficg8@gmail.com>
2020-06-09 19:20:23 +0100

    login save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -114,49 +114,4 @@ export class AppComponent implements OnInit {
     
   }
 
-  initMenu( user_id, _db ){
-    
-    let role_ids;
-    _db.setCollectionName( 'users_role' );
-
-    _db.filter( 'user_id', 'in', user_id ).subscribe( data => {
-      role_ids = data.map( e => {
-        return e.payload.doc.data()[ 'role_id' ];
-      })
-    });
-
-    let id_role;
-    _db.setCollectionName( 'role' );
-
-    _db.filter( 'id', 'in', role_ids ).subscribe( data => {
-      id_role = data.map( e => {
-        return e.payload.doc.data()[ 'role_id' ];
-      })
-    });
-
-    let menu_ids;
-    _db.setCollectionName( 'menu_role' );
-
-    _db.filter( 'role_id', 'in', id_role ).subscribe( data => {
-      menu_ids = data.map( e => {
-        return e.payload.doc.data()[ 'role_id' ];
-      })
-    });
-
-
-    _db.setCollectionName( 'menu' );
-    _db.filter( 'id', 'in', menu_ids ).subscribe( data => {
-      this.appPages = data.map( e => {
-        return {
-          title : e.payload.doc.data()[ 'title' ],
-          path : e.payload.doc.data()[ 'path' ],
-          url  : e.payload.doc.data()[ 'url' ],
-          parent_id  : e.payload.doc.data()[ 'parent_id' ],
-          icon : e.payload.doc.data()[ 'icon' ],
-        };
-      })
-
-    });
-  }
-
 }
